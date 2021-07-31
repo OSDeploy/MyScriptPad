ec2eb9821ae32bc1246c7fc0e5b55a929a7c0798
David Segura <david@segura.org>
2021-07-31 14:23:00 -0500

    21.7.31.1

--- Scripts/150fd29e-95e7-4675-8ade-4b675cd6343f.ps1
+++
@@ -1,4 +0,0 @@
-New-OSDCloud.template -WinRE
-New-OSDCloud.workspace -WorkspacePath 'C:\OSDCloud'
-Edit-OSDCloud.winpe -CloudDriver Dell,WiFi -Wallpaper 
-New-OSDCloud.iso
---
+++ Scripts/92aa62bf-c791-4651-90a5-70219a359e87.ps1
@@ -0,0 +1,6 @@
+#   GitHib User:        OSDeploy
+#   GitHub Repository:  MyScriptPad
+#   ScriptPad Index:    ScriptPad.json
+#   https://raw.githubusercontent.com/OSDeploy/MyScriptPad/main/ScriptPad.json
+
+Start-ScriptPad -MyScriptPad OSDeploy
